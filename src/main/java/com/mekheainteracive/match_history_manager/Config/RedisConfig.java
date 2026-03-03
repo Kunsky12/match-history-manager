9ec786b0a83dbbc3124bb3568a1e66896cb55ad2
Kunsky12 <77968149+Kunsky12@users.noreply.github.com>
2026-03-03 13:09:54 +0700

    3/3

---
+++ src/main/java/com/mekheainteracive/match_history_manager/Config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.mekheainteracive.match_history_manager.Config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig())
+                .build();
+    }
+}
